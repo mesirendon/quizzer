59879be7dbf1cfba2b7e783f80c1f1e12c6841e7
Mesi Rendon <amrendonsa@unal.edu.co>
2020-11-24 22:59:33 -0500

    Adding basic behavior
            I've added the basic styling using bootstrap and the basic
            behavior using metamask in order to get access to the account.
            Some rendering is being shown depending on the login ofthe
            accouunt.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>quiZZer</h1>
+    <p>quiZZer ha sido construido por <a href="https://twitter.com/mesirendon" target="_blank">@mesirendon</a>
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>quiZZer</h1>
+    <div v-if="isLogged"></div>
+    <div v-else>
+      <p>quiZZer es una ÐApp (aplicación descentralizada) que permite a profesores y alumnos
+        interactuar con quizzes de preguntas cerradas. Basta con tener una cuenta de ETH usando
+        Metamask con suficientes fondos en la red de pruebas de Ropsten.</p>
+      <p>quiZZer permite a los profesores crear un quiz como contrato inteligente estableciendo la
+        descripción y preguntas con sus opciones y respuesta correcta.</p>
+      <p>Los estudiantes, quienes deben tener una cuenta en la red de Ropste, pueden acceder al quiz
+        y contestar las preguntas. <span class="text-danger">Solo hay un intento.</span></p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { mapGetters } from 'vuex';
+import * as constants from '@/store/constants';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  computed: {
+    ...mapGetters({
+      isLogged: constants.SESSION_IS_LOGGED,
+    }),
   },
 };
 </script>
